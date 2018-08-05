55256798500f3afd4c020cdd878566ada6dc240b
kingDuiDui <m18508218948@163.com>
2018-08-06 00:54:05 +0800

    rmdir

--- fs/dir/4.dir.js
+++ fs/dir/4.dir.js
@@ -4,18 +4,59 @@ const fs = require('fs');
 const path = require('path');
 
 // 先序广度遍历
-function remdirSync(dir) {
-    let arr = [dir];
-    let index = 0;
-    let current = 0;
-    while (current = arr[index++]) {
-        let dirs = fs.readdirSync(current).map(i => path.join(current, i));
-        arr = [...arr, ...dirs];
-    }
-    for(let i = arr.length-1;i>=0;i--){
-        fs.rmdirSync(arr[i]);
-    }
+// function remdirSync(dir) {
+//     let arr = [dir];
+//     let index = 0;
+//     let current = 0;
+//     while (current = arr[index++]) {
+//         let dirs = fs.readdirSync(current).map(i => path.join(current, i));
+//         arr = [...arr, ...dirs];
+//     }
+//     for (let i = arr.length - 1; i >= 0; i--) {
+//         fs.rmdirSync(arr[i]);
+//     }
 
+// }
+
+// function remdir(dir, cb) {
+//     fs.readdir(dir, (err, files) => {
+//         let paths = files.map((item) => path.join(dir, item));
+//         if(paths.length>0){
+//             paths.forEach((i)=>{
+//                 function next(){
+
+//                 }
+//                 remdir(i,next)
+//             });
+//         }else{
+//             fs.rmdir(dir,cb)
+//         }
+//     });
+// }
+
+// remdir('a', () => {
+//     console.log('删除成功');
+// });
+
+function removeDir(dir,cb) { 
+    fs.readdir(dir,(err,files)=>{
+        let paths = files.map(file=>path.join(dir,file));
+        function next(index) {
+        // 第一次取出的是a/1.js
+        if (index === paths.length) return fs.rmdir(dir,cb);
+        let currentPath = paths[index];
+        // 文件删除后继续拿出下一项 继续删除
+        // 串行删除，删除完第一个，第一个删除完后调用第二个删除的方法
+        removeDir(currentPath,()=>next(index+1));
+        }
+        next(0);
+    })
 }
+removeDir('a',()=>{
+  console.log('删除成功');
+});
 
-remdirSync('a');
+
+
+
+
---
+++ stream/1.case.js
@@ -0,0 +1,31 @@
+// open  data end error close
+
+const fs = require('fs');
+let rs = fs.createReadStream('./a.txt',{
+    flags:'r',
+    encoding:null,
+    mode:0o666,
+    start:0,
+    end:7,
+    highWaterMark:3,
+    autoClose:true
+})
+
+rs.on('open',()=>{
+    console.log('open');
+});
+rs.on('data',(data)=>{
+    console.log(data.toString());
+});
+rs.on('end',()=>{
+    console.log('end');
+});
+rs.on('close',()=>{
+    console.log('close');
+});
+rs.on('error',(err)=>{
+    console.log(`err:${err}`);
+});
+
+
+
